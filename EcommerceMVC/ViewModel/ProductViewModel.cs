8dae11a71efd0d992b2dae97fee77f344f76e778
praveen praveen <praveensumathi98@gmail.com>
2021-04-02 01:27:53 +0530

    add admin cshtml

--- EcommerceMVC/ViewModel/ProductViewModel.cs
+++ EcommerceMVC/ViewModel/ProductViewModel.cs
@@ -1,13 +1,23 @@
 ﻿using EcommerceMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EcommerceMVC.ViewModel
 {
-    public class ProductViewModel
+    public class ProductViewModel : ProductInpuModel
     {
         public List<Product> Products { get; set; }
+    }
+
+    public class ProductInpuModel
+    {
+
+        [Required]
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
     }
 }
